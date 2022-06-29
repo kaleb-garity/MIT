36f2adfe28e5362f6537a76d0e754239db7e171e
kaleb-garity <ktgarity@gmail.com>
2022-06-29 16:26:38 -0600

    Finished pset 1 and 2

--- pset1b.py
+++ pset1b.py
@@ -1,6 +1,7 @@
 annual_salary = float(input("Enter your annual salary: "))
 portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
 total_cost = float(input("Enter the cost of your dream home: "))
+semi_annual_raise = float(input("Enter the semi-annual raise, as a decimal: "))
 
 portion_down_payment = 0.25
 down_payment = total_cost * portion_down_payment
@@ -11,5 +12,8 @@ total_months = 0
 while current_savings < down_payment:
     total_months += 1
     current_savings += (annual_salary / 12 * portion_saved) + (current_savings * r / 12)
+
+    if total_months % 6 == 0:
+            annual_salary = annual_salary * (semi_annual_raise + 1)
 
 print("Number of months:", total_months)
